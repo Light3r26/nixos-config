75a4444718e4a376c5aa04e30977581e7cff5517
Light3r26 <soria.jacopo@gmail.com>
2025-06-03 00:25:10 +0200

    sostituzione wofi con rofi

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -3,21 +3,40 @@
 {
   programs.hyprland.enable = true;
 
+    # Useful for Steam, Proton, ecc.
+  hardware.graphics.enable = true;
+
   environment.sessionVariables = {
     XDG_SESSION_TYPE = "wayland";
     XDG_CURRENT_DESKTOP = "Hyprland";
-    # Prevent cursor from becoming invisible
     WLR_NO_HARDWARE_CURSORS = "1";
     # Hint electron apps to use Wayland
     NIXOS_OZONE_WL = "1";
+    # Nvidia settings for cursor on second monitor
+    #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    #DRI_PRIME = "1";
+    # Prevent cursor from becoming invisible
   };
+  
+  # Activates NVIDIA drivers
+  #services.xserver.videoDrivers = [ "nvidia" ];
+
+  #hardware.nvidia = {
+    #modesetting.enable = true;
+    #powerManagement.enable = true;
+    #nvidiaSettings = true;
+    #open = false; 
+  #};
+
+  # Needed for fluid cursor with NVIDIA videoDrivers
+  #boot.kernelParams = [ "nvidia-drm.modeset=1" ];
 
   environment.systemPackages = with pkgs; [
     wayland
     xwayland
     kitty
     waybar
-    wofi
+    rofi
     kdePackages.dolphin # File manager
     hyprshot # Screenshot utility
     jq # JSON parser and manipulator
