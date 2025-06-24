d2fd93d0053ff705df9e110ad46497efce98a2f8
Light3r26 <soria.jacopo@gmail.com>
2025-06-24 02:13:39 +0200

    ultima prova con AccentColor

--- modules/nixos/sddm/sddm.nix
+++ modules/nixos/sddm/sddm.nix
@@ -12,9 +12,9 @@
       FormPosition = "center";
       PartialBlur = true;
       BlurRadius = 50;
-      MainColor = "#999FAB";
+      MainColor = "#3B414D";#3B414D
       BackgroundColor = "#8D939F";
-      #AccentColor = "#999FAB";#3B414D
+      AccentColor = "#999FAB";
     };
   };
 }
