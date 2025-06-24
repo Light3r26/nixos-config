f199df3b9f3f604a8bdbca61d5ee32197d2c1552
Light3r26 <soria.jacopo@gmail.com>
2025-06-24 02:16:55 +0200

    fix cazzatona sbagliata non avevo messo in settings

--- modules/nixos/sddm/sddm.nix
+++ modules/nixos/sddm/sddm.nix
@@ -6,15 +6,17 @@
     wayland.enable = true;
     sugarCandyNix = {
       enable = true;
-      Background = lib.cleanSource ./bright-mountains.jpg;
-      ScreenWidth = 1920;
-      ScreenHeight = 1080;
-      FormPosition = "center";
-      PartialBlur = true;
-      BlurRadius = 50;
-      MainColor = "#999FAB";#3B414D
-      BackgroundColor = "#8D939F";
-      #AccentColor = "#999FAB";
+      settings = {
+        Background = lib.cleanSource ./bright-mountains.jpg;
+        ScreenWidth = 1920;
+        ScreenHeight = 1080;
+        FormPosition = "center";
+        PartialBlur = true;
+        BlurRadius = 50;
+        MainColor = "#3B414D";
+        BackgroundColor = "#8D939F";
+        AccentColor = "#999FAB";
+      };
     };
   };
 }
