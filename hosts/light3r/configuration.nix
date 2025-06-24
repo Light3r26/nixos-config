66711a8ce57ff706c22486676363206028d18bba
Light3r26 <soria.jacopo@gmail.com>
2025-06-24 23:22:12 +0200

    Aggiunta supporto per esecuzione di binaries

--- hosts/light3r/configuration.nix
+++ hosts/light3r/configuration.nix
@@ -101,6 +101,15 @@
      sops
   ];
 
+  programs.nix-ld = {
+    enable = true; 
+    libraries = with pkgs; [
+      # Add any missing dynamic libraries for unpackaged
+      # programs here, NOT in environment.systemPackages
+    ];
+
+  };
+
   fonts.packages = with pkgs; [
     font-awesome
     nerd-fonts.caskaydia-cove
@@ -110,11 +119,5 @@
   # Enabled services
   services.openssh.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Do not change
 }
