0ab74bb4eb2cb3a9371a77a0bf70faf0e8d258e9
Light3r26 <soria.jacopo@gmail.com>
2025-06-25 00:28:46 +0200

    fix percorso in modulo home manager

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -7,7 +7,7 @@
     users = {
       "light3r" = { 
         imports = [
-          ../../hosts/light3r/home.nix
+          ../home-manager/home.nix
           #inputs.self.outputs.homeManagerModules.default
         ];
       };
