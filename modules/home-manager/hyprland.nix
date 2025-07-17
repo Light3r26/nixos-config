14142c0c143ad32c1d526db5a60904147c42c2a3
Light3r26 <soria.jacopo@gmail.com>
2025-07-18 01:11:20 +0200

    fix di modulo home manager di hyprland

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -3,35 +3,7 @@
 {
   # Hyprland enabled in configuration.nix to assure sistem-level-changes 
 
-  # Useful for Steam, Proton, ecc.
-  hardware.graphics.enable = true;
-
-  environment.sessionVariables = {
-    XDG_SESSION_TYPE = "wayland";
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    # Hint electron apps to use Wayland
-    NIXOS_OZONE_WL = "1";
-    # Nvidia settings for cursor on second monitor
-    #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    #DRI_PRIME = "1";
-    # Prevent cursor from becoming invisible
-  };
-  
-  # Activates NVIDIA drivers
-  #services.xserver.videoDrivers = [ "nvidia" ];
-
-  #hardware.nvidia = {
-    #modesetting.enable = true;
-    #powerManagement.enable = true;
-    #nvidiaSettings = true;
-    #open = false; 
-  #};
-
-  # Needed for fluid cursor with NVIDIA videoDrivers
-  #boot.kernelParams = [ "nvidia-drm.modeset=1" ];
-
-  environment.systemPackages = with pkgs; [
+  home.packages = with pkgs; [
     wayland
     xwayland
     waybar
