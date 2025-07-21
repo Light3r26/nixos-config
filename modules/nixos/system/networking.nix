40e887cb94504efc93da7a2d7acbc35a6ef0acb0
Light3r26 <soria.jacopo@gmail.com>
2025-07-21 21:15:42 +0200

    fix

--- modules/nixos/system/networking.nix
+++ modules/nixos/system/networking.nix
@@ -3,7 +3,6 @@
 {
   networking.hostName = "light3r"; # Define your hostname.
   networking.networkmanager.enable = true; # Enable networking
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
