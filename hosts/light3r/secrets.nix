c8135f72751a1f19759edd47388dc0c2f9c0ac1d
Light3r26 <soria.jacopo@gmail.com>
2025-08-06 01:20:29 +0200

    fix

--- hosts/light3r/secrets.nix
+++ hosts/light3r/secrets.nix
@@ -1,10 +1,10 @@
 {
   age = {
     secrets = {
-      #light3r-user-secrets = {
-      #file = ../../secrets/light3r-user-secrets.age;
-      #owner = "light3r";
-      #};
+      light3r-user-secrets = {
+        file = ../../secrets/light3r-user-secrets.age;
+        owner = "light3r";
+      };
     };
   };
 }
