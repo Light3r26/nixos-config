e92bd0312b2b410960f0f251d5351308d06aee9b
Light3r26 <soria.jacopo@gmail.com>
2025-08-06 01:53:17 +0200

    changed ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  light3r = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2MRoO0sHpm3T3vrnlJSk1HJJ333M7TD2P29aE4tkSH";
+  light3r = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRwZUMyc1pxcntOJXC+qShO6nSbvBEYwRQJCnk+S993";
 in {
   "light3r-user-secrets.age".publicKeys = [ light3r ];
 }
