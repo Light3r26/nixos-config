442be5670bda45ee239c370eeffb4502f1837530
Light3r26 <soria.jacopo@gmail.com>
2025-08-06 01:34:44 +0200

    trying to add the user secrets to the config

--- modules/nixos/system/users.nix
+++ modules/nixos/system/users.nix
@@ -1,10 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  age.secrets.light3r-user-secrets.file = ../../../secrets/light3r-user-secrets.age;
   # Define user accounts.
   users.users.light3r = {
     isNormalUser = true;
-    description = "Jacopo Soria";
+    description = config.age.secrets.light3r-user-secrets.path;
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
   };
