6e75136612600e1a77698f9de2518a9214d7b521
Light3r26 <soria.jacopo@gmail.com>
2025-08-07 17:04:19 +0200

    Mounts NAS only the first time it's been accessed, not at boot

--- modules/nixos/servers.nix
+++ modules/nixos/servers.nix
@@ -6,6 +6,6 @@
   fileSystems."/home/light3r/NAS-115J" = {
     device = "192.168.1.42:/volume1";
     fsType = "nfs";
-    #options = [ "x-systemd.automount" "noauto" ];
+    options = [ "x-systemd.automount" "noauto" ];
   };
 }
