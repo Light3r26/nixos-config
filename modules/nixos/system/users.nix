be8f4c8336805ef5b4023b6956a4ee49ef69a71b
Light3r26 <soria.jacopo@gmail.com>
2025-08-07 10:02:20 +0200

    going to try secrets afterwards

--- modules/nixos/system/users.nix
+++ modules/nixos/system/users.nix
@@ -1,11 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  age.secrets.light3r-user-secrets.file = ../../../secrets/light3r-user-secrets.age;
   # Define user accounts.
   users.users.light3r = {
     isNormalUser = true;
-    description = config.age.secrets.light3r-user-secrets.path;
+    description = "Jacopo Soria";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
   };
