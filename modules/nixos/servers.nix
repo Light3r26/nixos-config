4599974e7013f612bb4a151dfd59f5bac071109a
Light3r26 <soria.jacopo@gmail.com>
2025-08-07 16:57:44 +0200

    trying to let it work

--- modules/nixos/servers.nix
+++ modules/nixos/servers.nix
@@ -4,7 +4,7 @@
   # Going to add the NixOS server up here
 
   fileSystems."/home/light3r/NAS-115J" = {
-    device = "192.168.1.42:/NAS-115J";
+    device = "192.168.1.42:/volume1";
     fsType = "nfs";
     #options = [ "x-systemd.automount" "noauto" ];
   };
