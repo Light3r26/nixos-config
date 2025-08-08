90f6a2fe71335930c9723b9651c6f37ae1a9b254
Light3r26 <soria.jacopo@gmail.com>
2025-08-09 01:28:35 +0200

    fix

---
+++ modules/nixos/styling.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable QT
+  qt = {
+    enable = true;
+    platformTheme = "qt5ct";
+    style = {
+      package = pkgs.kdePackages.breeze;
+      name = "breeze";
+      #package = pkgs.utterly-nord-plasma;
+      #name = "Utterly Nord Plasma";
+    };
+  };
+
+  # Enable GTK
+  #gtk = {
+  #  enable = true;
+  #  theme = {
+  #    package = pkgs.adw-gtk3;
+  #    name = "adw-gtk3-dark";
+  #  };
+  #};
+}
