d073cc8d353435498bff177ede57362a5262ea81
Light3r26 <soria.jacopo@gmail.com>
2025-08-09 02:07:54 +0200

    changing gtk theme

--- modules/home-manager/styling.nix
+++ modules/home-manager/styling.nix
@@ -1,24 +1,22 @@
 { config, pkgs, ... }:
 
 {
+  # Enable GTK
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.dracula-theme;
+      name = "Dracula";
+    };
+  };
+
   # Enable QT
   qt = {
     enable = true;
-    platformTheme.name = "kde6";
+    platformTheme.name = "qtct";
     style = {
-      #package = pkgs.kdePackages.breeze;
-      #name = "breeze";
       package = pkgs.utterly-nord-plasma;
       name = "Utterly Nord Plasma";
     };
   };
-
-  # Enable GTK
-  #gtk = {
-  #  enable = true;
-  #  theme = {
-  #    package = pkgs.adw-gtk3;
-  #    name = "adw-gtk3-dark";
-  #  };
-  #};
 }
