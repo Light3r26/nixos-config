81e9c0df3a2450ef847d1d9e92d02a27118f6631
Light3r26 <soria.jacopo@gmail.com>
2025-08-10 00:16:29 +0200

    hoping that NVIDIA explicit sync will resolve everything and switching back to default config

--- hosts/light3r/configuration.nix
+++ hosts/light3r/configuration.nix
@@ -28,47 +28,13 @@
   environment.sessionVariables = {
     XDG_SESSION_TYPE = "wayland";
     XDG_CURRENT_DESKTOP = "Hyprland";
+    # Prevent cursor from becoming invisible
     WLR_NO_HARDWARE_CURSORS = "1";
     # Hint electron apps to use Wayland
     NIXOS_OZONE_WL = "1";
-    # Nvidia settings for cursor on second monitor
-    #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    #DRI_PRIME = "1";
-    # Prevent cursor from becoming invisible
   };
   
-  # Activating NVIDIA drivers - start
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    open = true;
-
-    # Enable the Nvidia settings menu, accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    #package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  # Enable Steam
+    # Enable Steam
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
@@ -87,18 +53,8 @@
      starship
      nix-prefetch
      nix-prefetch-github
-     sops
   ];
 
-  programs.nix-ld = {
-    enable = true; 
-    libraries = with pkgs; [
-      # Add any missing dynamic libraries for unpackaged
-      # programs here, NOT in environment.systemPackages
-    ];
-
-  };
-    
   # Enabled services
   services.openssh.enable = true;
 
