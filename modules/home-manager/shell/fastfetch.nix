42cf85d9764438ed3f6e351690111456fb4ae197
Light3r26 <soria.jacopo@gmail.com>
2025-08-17 21:13:57 +0200

    added packag3

--- modules/home-manager/shell/fastfetch.nix
+++ modules/home-manager/shell/fastfetch.nix
@@ -1,8 +1,9 @@
-{ config, ... } =
+{ config, pkgs, ... } =
 
 {
   programs.fastfetch = {
     enable = true;
+    package = pkgs.fastfetch;
     settings = {
       "$schema" = "https:#github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
     
