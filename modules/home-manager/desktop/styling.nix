ef404c1ab629f0dfb105fcbbc9ce9d7fbfca9bc3
Light3r26 <soria.jacopo@gmail.com>
2025-08-17 23:01:30 +0200

    wrong name

--- modules/home-manager/desktop/styling.nix
+++ modules/home-manager/desktop/styling.nix
@@ -10,7 +10,7 @@
     };
     iconTheme = {
       package = pkgs.windows10-icons;
-      name = "Windows10-icons";
+      name = "Windows10";
     };
     cursorTheme = {
       package = pkgs.whitesur-cursors;
