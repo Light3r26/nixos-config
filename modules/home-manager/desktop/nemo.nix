afc4a64d1f45d7836950399e3aec00cd7975d3e1
Light3r26 <soria.jacopo@gmail.com>
2025-08-28 12:14:54 +0200

    fixing and documenting

--- modules/home-manager/desktop/nemo.nix
+++ modules/home-manager/desktop/nemo.nix
@@ -4,12 +4,27 @@
   home.packages = with pkgs; [
     nemo
   ];
+  
+  # Set Nemo as default file browser
+  xdg.desktopEntries.nemo = {
+    name = "Nemo";
+    exec = "${pkgs.nemo-with-extensions}/bin/nemo";
+  };
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+        "inode/directory" = [ "nemo.desktop" ];
+        "application/x-gnome-saved-search" = [ "nemo.desktop" ];
+    };
+  };
 
   dconf = {
     settings = {
+      # Set Kitty as default Nemo terminal
       "org/cinnamon/desktop/application/terminal" = {
         exec = "kitty";
       };
+      # Needed to change keyboard shortcuts 
       "org/cinnamon/desktop/interface" = {
         can-change-accels = true;
       };
@@ -17,6 +32,7 @@
   };
 
   home.file = {
+    # Set keyboard shortcut to open terminal
     ".gnome2/accels/nemo".text = ''
     (gtk_accel_path "<Actions>/DirViewActions/OpenInTerminal" "<Shift>d")
     '';
