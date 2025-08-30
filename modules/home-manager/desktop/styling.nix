7c6cf70ed64aa1e241eed464a1105ee6900b5732
Light3r26 <soria.jacopo@gmail.com>
2025-08-30 22:28:39 +0200

    trying later

--- modules/home-manager/desktop/styling.nix
+++ modules/home-manager/desktop/styling.nix
@@ -24,22 +24,8 @@
   qt = {
     enable = true;
     platformTheme.name = "qtct";
-    style = {
-      name = "kvantum";
-    };
-  };
-
-  home.packages = with pkgs; [
-    # Required for the Kvantum style engine.
-    libsForQt5.qtstyleplugin-kvantum
-  ];
-
-  xdg.configFile = {
-    "Kvantum/kvantum.kvconfig".text = ''
-      [General]
-      theme=Nordic-Darker-Solid
-    '';
-
-    "Kvantum/Nordic".source = "${pkgs.nordic}/share/Kvantum/Nordic";
+    #style = {
+    #  name = "kvantum";
+    #};
   };
 }
