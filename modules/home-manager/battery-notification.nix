1cd17b19b8cf79efee314c5f3cca7e2f10f560da
Light3r26 <soria.jacopo@gmail.com>
2025-09-01 17:35:42 +0200

    should fix it

--- modules/home-manager/battery-notification.nix
+++ modules/home-manager/battery-notification.nix
@@ -5,18 +5,18 @@
 # Script made by sanjivthapasvt on github in his Low-Battery-Notification-Script repo
 
 let
-  batteryNotification = pkgs.writeScriptBin "battery-notification" ''
-    #!/bin/bash
+  batteryNotification = pkgs.writeScript "battery-notification" ''
+    #!/usr/bin/env bash
     notified=0
 
     while true; do
       battery_path=$(find /sys/class/power_supply/ -name 'BAT*' | head -n1)
-      battery_level=$(cat "$\{battery_path}/capacity")
-      charging_status=$(cat "$\{battery_path}/status")
+      battery_level=$(cat "$battery_path/capacity")
+      charging_status=$(cat "$battery_path/status")
 
       if [[ "$battery_level" -le 30 && "$charging_status" != "Charging" ]]; then
         if [[ $notified -eq 0 ]]; then
-          notify-send "⚠️ Battery Low" "Battery level is at ${battery_level}%!" -u critical
+          notify-send "⚠️ Battery Low" "Battery level is at ''${battery_level}%!" -u critical
           notified=1
         fi
       else
