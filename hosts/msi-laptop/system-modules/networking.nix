9deb5c64189f4a1c69e8aa6d8570a3d7866fe6c7
Light3r26 <soria.jacopo@gmail.com>
2025-09-09 22:55:40 +0200

    adding static ip to stop NAS mounting issues

--- hosts/msi-laptop/system-modules/networking.nix
+++ hosts/msi-laptop/system-modules/networking.nix
@@ -9,6 +9,13 @@
     allowPing = true;
   };
 
+  networking.interfaces.wlo1.ipv4.addresses = [
+    {
+      address = "192.168.1.34";
+      prefixLength = 24;
+    }
+  ];
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
