fa752fbd8e2192379f0bf46ab97c0bd8214d8b11
Light3r26 <soria.jacopo@gmail.com>
2025-09-09 22:59:07 +0200

    disabled dhcpd

--- hosts/msi-laptop/system-modules/networking.nix
+++ hosts/msi-laptop/system-modules/networking.nix
@@ -1,22 +1,25 @@
 { config, ... }:
 
 {
-  networking.hostName = "msi-laptop"; # Define your hostname.
-  networking.networkmanager.enable = true; # Enable networking
+  networking = {
+    hostName = "msi-laptop"; # Define your hostname.
+    networkmanager.enable = true; # Enable networking
 
-  networking.firewall = {
-    enable = true;
-    allowPing = true;
+    firewall = {
+      enable = true;
+      allowPing = true;
+    };
+
+    dhcpcd.enable = false;
+    interfaces.wlo1.ipv4.addresses = [
+      {
+        address = "192.168.1.34";
+        prefixLength = 24;
+      }
+    ];
+
+    # Configure network proxy if necessary
+    # proxy.default = "http://user:password@proxy:port/"; 
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   };
-
-  networking.interfaces.wlo1.ipv4.addresses = [
-    {
-      address = "192.168.1.34";
-      prefixLength = 24;
-    }
-  ];
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 }
