df50cc8da07874e05bf0623086b0af83dc897725
Light3r26 <soria.jacopo@gmail.com>
2025-09-13 11:29:28 +0200

    removing static ip

--- hosts/msi-laptop/system-modules/networking.nix
+++ hosts/msi-laptop/system-modules/networking.nix
@@ -10,14 +10,6 @@
       allowPing = true;
     };
 
-    dhcpcd.enable = false;
-    interfaces.wlo1.ipv4.addresses = [
-      {
-        address = "192.168.1.34";
-        prefixLength = 24;
-      }
-    ];
-
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/"; 
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
