4c4ef8a93116172b93f398d22c2478d9426485ac
Light3r26 <soria.jacopo@gmail.com>
2025-09-14 12:20:39 +0200

    some other changes

--- modules/home-manager/desktop/hypridle.nix
+++ modules/home-manager/desktop/hypridle.nix
@@ -12,21 +12,20 @@
 
       listener = [ 
         {
-          timeout = 240;                                # 4min.
+          timeout = 300;                                 # 5min
+          on-timeout = "loginctl lock-session";            # lock screen when timeout has passed
+        }
+
+        {
+          timeout = 900;                                # 15min.
           on-timeout = "brightnessctl -s set 10";         # set monitor backlight to minimum, avoid 0 on OLED monitor.
           on-resume = "brightnessctl -r";                 # monitor backlight restore.
         }
 
-        # turn off keyboard backlight, comment out this section if you dont have a keyboard backlight.
         { 
-          timeout = 240;                                         # 4min.
+          timeout = 900;                                         # 15min.
           on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
           on-resume = "brightnessctl -rd rgb:kbd_backlight";       # turn on keyboard backlight.
-        }
-
-        {
-          timeout = 300;                                 # 5min
-          on-timeout = "loginctl lock-session";            # lock screen when timeout has passed
         }
 
         {
