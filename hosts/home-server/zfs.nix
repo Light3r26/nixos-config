74d163e84c93b809280682a2c8b98cc3e5d6d4c7
Light3r26 <soria.jacopo@pm.me>
2025-10-05 01:41:59 +0200

    added periodic zfs pool scrub and snapshotting

--- hosts/home-server/zfs.nix
+++ hosts/home-server/zfs.nix
@@ -5,4 +5,24 @@
   boot.zfs.forceImportRoot = false; 
 
   age.secrets."zfs_key.age".file = "/Nixos/secrets/zfs_key.age";
+
+  services.zfs.autoScrub = {
+    enable = true;
+    interval = "weekly";
+  };
+
+  services.sanoid = {
+    enable = true;
+    templates.backup = {
+      hourly = 36;
+      daily = 30;
+      monthly = 3;
+      autoprune = true;
+      autosnap = true;
+    };
+
+    datasets."taskpool/services" = {
+      useTemplate = [ "backup" ];
+    };
+  };
 }
