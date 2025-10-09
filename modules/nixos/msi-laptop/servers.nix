fe51367244063bfc48a5306e3a4daa4160f95d16
Light3r26 <soria.jacopo@gmail.com>
2025-10-09 22:25:00 +0200

    adding nixos server mount in laptop config

--- modules/nixos/msi-laptop/servers.nix
+++ modules/nixos/msi-laptop/servers.nix
@@ -1,15 +1,15 @@
 { config , ... }:
 
 {
-  #fileSystems."/home/light3r/Home-Server/Photos" = {
-  #  device = "192.168.1.56:/photos";
-  #  fsType = "nfs";
-  #  options = [ 
-  #    "_netdev"
-  #    "nofail"
-  #    "rw"
-  #  ];
-  #};
+  fileSystems."/home/light3r/Home-Server/Photos" = {
+    device = "192.168.1.56:/photos";
+    fsType = "nfs";
+    options = [ 
+      "_netdev"
+      "nofail"
+      "rw"
+    ];
+  };
 
   fileSystems."/home/light3r/NAS-115J" = {
     device = "192.168.1.42:/volume1";
