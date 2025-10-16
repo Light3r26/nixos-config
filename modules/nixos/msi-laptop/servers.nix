aa8a32ca3b86d2ca44a63e7537948c0146a5585b
Light3r26 <soria.jacopo@gmail.com>
2025-10-16 23:42:38 +0200

    changed some user side nfs things

--- modules/nixos/msi-laptop/servers.nix
+++ modules/nixos/msi-laptop/servers.nix
@@ -2,13 +2,13 @@
 
 {
   fileSystems."/home/light3r/Home-Server" = {
-    device = "192.168.1.56:/home-pool";
-    fsType = "nfs";
-    options = [ 
-      "_netdev"
-      "nofail"
-      "rw"
-    ];
+   device = "192.168.1.56:/home-pool";
+   fsType = "nfs";
+   options = [ 
+     "_netdev"
+     "nofail"
+     "rw"
+   ];
   };
 
   fileSystems."/home/light3r/NAS-115J" = {
