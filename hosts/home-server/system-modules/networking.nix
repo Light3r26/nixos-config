602f93361665954a17decaef8229e21db09a0d07
Light3r26 <soria.jacopo@pm.me>
2025-10-16 23:39:20 +0200

    some networking changes

--- hosts/home-server/system-modules/networking.nix
+++ hosts/home-server/system-modules/networking.nix
@@ -6,8 +6,8 @@
   networking.hostId = "007f0200";
 
   networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ 80 443 2049 4000 4001 4002 ];
-  networking.firewall.allowedUDPPorts = [ 2049 4000 4001 4002 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 2049 ];
+  networking.firewall.allowedUDPPorts = [ 2049 ];
 
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
