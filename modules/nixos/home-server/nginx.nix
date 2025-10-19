000c80b4ecf30f0f2e12d123613d5c504b118a34
Light3r26 <soria.jacopo@gmail.com>
2025-10-19 10:50:40 +0200

    added music downloading script

--- modules/nixos/home-server/nginx.nix
+++ modules/nixos/home-server/nginx.nix
@@ -3,25 +3,27 @@
 {
   services.nginx = {
     enable = true;
-    virtualHosts."jacoposoria.qzz.io" = {
-      serverName = "jacoposoria.qzz.io";
-      root = "/var/www/jacoposoria.qzz.io";
-      listen = [ { addr = "127.0.0.1"; port = 80; } ];
-    };
-    virtualHosts."music.jacoposoria.qzz.io" = {
-      serverName = "music.jacoposoria.qzz.io";
-      listen = [ { addr = "127.0.0.1"; port = 80; } ];
-      locations."/" = {
-      	proxyPass = "http://127.0.0.1:4533";
-      	proxyWebsockets = true;
-    	extraConfig = ''
-  	  proxy_redirect off;
-          proxy_set_header Host $host:$server_port;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Host $server_name;
-          proxy_set_header X-Forwarded-Proto $scheme;
-        '';
+    virtualHosts = {
+      "jacoposoria.qzz.io" = {
+        serverName = "jacoposoria.qzz.io";
+        root = "/var/www/jacoposoria.qzz.io";
+        listen = [ { addr = "127.0.0.1"; port = 80; } ];
+      };
+      "music.jacoposoria.qzz.io" = {
+        serverName = "music.jacoposoria.qzz.io";
+        listen = [ { addr = "127.0.0.1"; port = 80; } ];
+        locations."/" = {
+      	  proxyPass = "http://127.0.0.1:4533";
+      	  proxyWebsockets = true;
+    	    extraConfig = ''
+  	        proxy_redirect off;
+            proxy_set_header Host $host:$server_port;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Host $server_name;
+            proxy_set_header X-Forwarded-Proto $scheme;
+          '';
+        };
       };
     };
   };
