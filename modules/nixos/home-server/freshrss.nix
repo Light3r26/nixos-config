e8854984c0d8ca187f7fcbbc0845655931cdb679
Light3r26 <soria.jacopo@pm.me>
2025-11-02 00:10:48 +0100

    fix: changed owner of the freshrss secret

--- modules/nixos/home-server/freshrss.nix
+++ modules/nixos/home-server/freshrss.nix
@@ -6,9 +6,14 @@
     baseUrl = "/";
     virtualHost = "rss.jacoposoria.qzz.io";
     language = "it";
-    defaultUser = "light3r";
+    #defaultUser = "light3r";
     passwordFile = config.age.secrets."freshrss_key.age".path;
   };
 
-  age.secrets."freshrss_key.age".file = "/Nixos/secrets/freshrss_key.age";
+  age.secrets."freshrss_key.age" = {
+    file = "/Nixos/secrets/freshrss_key.age";
+    owner = "freshrss";
+    group = "freshrss";
+    mode = "0600";
+  };
 }
