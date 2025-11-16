d992ce023fec1aaedc66dc4627b33c6ba624ad49
Light3r26 <soria.jacopo@pm.me>
2025-11-16 18:21:46 +0100

    trying to get remote build to work

--- modules/nixos/home-server/remote-builder.nix
+++ modules/nixos/home-server/remote-builder.nix
@@ -1,11 +1,29 @@
 {
   users.users.remotebuild = {
-    isSystemUser = true;
+    isNormalUser = true;
+    createHome = false;
     group = "remotebuild";
-    useDefaultShell = true;
     openssh.authorizedKeys.keyFiles = [ ./remotebuild.pub ]; 
   };
   
   users.groups.remotebuild = {};
-  nix.settings.trusted-users = [ "remotebuild" ];
+
+  nix = {
+     nrBuildUsers = 64;
+     settings = {
+       trusted-users = [ "remotebuild" ];
+ 
+       min-free = 10 * 1024 * 1024;
+       max-free = 200 * 1024 * 1024;
+
+       max-jobs = "auto";
+       cores = 0;
+     };
+   };
+
+   systemd.services.nix-daemon.serviceConfig = {
+     MemoryAccounting = true;
+     MemoryMax = "90%";
+     OOMScoreAdjust = 500;
+   };
 }
