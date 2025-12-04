f7d388293ab401ad016a18b7f5c554dd8ee34858
Light3r26 <soria.jacopo@pm.me>
2025-12-04 23:28:12 +0100

    floorp: settings configuration

--- modules/home-manager/apps/floorp.nix
+++ modules/home-manager/apps/floorp.nix
@@ -1,8 +1,21 @@
+let
+  lock-false = {
+    Value = false;
+    Status = "locked";
+  };
+
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
+
+in
 {
   programs.floorp = {
     enable = true;
     languagePacks = [ "it" "en-GB" ];
 
+    /* ---- PROFILES ---- */
     profiles.Light3r = {
       name = "Light3r";
       bookmarks = {
@@ -38,6 +51,29 @@
       };
     };
 
+    /* ---- PREFERENCES ---- */
+    # Check about:config for options.
+    Preferences = { 
+      "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+      "extensions.pocket.enabled" = lock-false;
+      "extensions.screenshots.disabled" = lock-true;
+      "browser.topsites.contile.enabled" = lock-false;
+      "browser.formfill.enable" = lock-false;
+      "browser.search.suggest.enabled" = lock-false;
+      "browser.search.suggest.enabled.private" = lock-false;
+      "browser.urlbar.suggest.searches" = lock-false;
+      "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+      "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+      "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+      "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+      "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+      "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+      "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+      "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+      "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+      "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+    };
+
     /* ---- POLICIES ---- */
     # Check about:policies#documentation for options.
     policies = {
@@ -57,7 +93,7 @@
       OverridePostUpdatePage = "";
       DontCheckDefaultBrowser = true;
       DisplayBookmarksToolbar = "always"; # alternatives: "never" or "newtab"
-      DisplayMenuBar = "always"; # alternatives: "default-off", "never" or "default-on"
+      DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
       SearchBar = "unified"; # alternative: "separate"
     };
   };
