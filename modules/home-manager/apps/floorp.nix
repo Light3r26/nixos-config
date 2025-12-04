6bc8bae43e8ae60308c660db3e13c52864626b24
Light3r26 <soria.jacopo@pm.me>
2025-12-04 23:31:58 +0100

    code fix

--- modules/home-manager/apps/floorp.nix
+++ modules/home-manager/apps/floorp.nix
@@ -49,29 +49,28 @@ in
           }
         ];
       };
-    };
-
-    /* ---- PREFERENCES ---- */
-    # Check about:config for options.
-    Preferences = { 
-      "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
-      "extensions.pocket.enabled" = lock-false;
-      "extensions.screenshots.disabled" = lock-true;
-      "browser.topsites.contile.enabled" = lock-false;
-      "browser.formfill.enable" = lock-false;
-      "browser.search.suggest.enabled" = lock-false;
-      "browser.search.suggest.enabled.private" = lock-false;
-      "browser.urlbar.suggest.searches" = lock-false;
-      "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
-      "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
-      "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
-      "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
-      "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
-      "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
-      "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
-      "browser.newtabpage.activity-stream.showSponsored" = lock-false;
-      "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
-      "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+      settings = {
+        /* ---- PREFERENCES ---- */
+        # Check about:config for options.
+        "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+        "extensions.pocket.enabled" = lock-false;
+        "extensions.screenshots.disabled" = lock-true;
+        "browser.topsites.contile.enabled" = lock-false;
+        "browser.formfill.enable" = lock-false;
+        "browser.search.suggest.enabled" = lock-false;
+        "browser.search.suggest.enabled.private" = lock-false;
+        "browser.urlbar.suggest.searches" = lock-false;
+        "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+      };
     };
 
     /* ---- POLICIES ---- */
