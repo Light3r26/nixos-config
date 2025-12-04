c8aa961102dbe3b8ecd1f3889c96cc89aaf74332
Light3r26 <soria.jacopo@pm.me>
2025-12-05 00:03:18 +0100

    code fix

--- modules/home-manager/apps/floorp.nix
+++ modules/home-manager/apps/floorp.nix
@@ -49,9 +49,41 @@ in
           }
         ];
       };
+
+      /* ---- EXTENSIONS ---- */
+      # Check about:support for extension/add-on ID strings.
+      # Valid strings for installation_mode are "allowed", "blocked",
+      # "force_installed" and "normal_installed".
+      extensions = {
+        force = true;
+              settings = {
+          "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+          # Nord Theme:
+          "26e789e7-acf2-4346-9381-ad473c245e43" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/nord-theme/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          # Dark Reader:
+          "addon@darkreader.org" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          # AdNauseam:
+          "adnauseam@rednoise.org" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/adnauseam/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          # Sponsor Block:
+          "sponsorBlocker@ajay.app" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+      };
+
+      /* ---- SETTINGS ---- */
+      # Check about:config for options.
       settings = {
-        /* ---- PREFERENCES ---- */
-        # Check about:config for options.
         "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
         "extensions.pocket.enabled" = lock-false;
         "extensions.screenshots.disabled" = lock-true;
@@ -70,37 +102,6 @@ in
         "browser.newtabpage.activity-stream.showSponsored" = lock-false;
         "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
         "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
-      };
-    };
-
-    extensions = {
-      force = true;
-      /* ---- EXTENSIONS ---- */
-      # Check about:support for extension/add-on ID strings.
-      # Valid strings for installation_mode are "allowed", "blocked",
-      # "force_installed" and "normal_installed".
-      settings = {
-        "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
-        # Nord Theme:
-        "26e789e7-acf2-4346-9381-ad473c245e43" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/nord-theme/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        # Dark Reader:
-        "addon@darkreader.org" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        # AdNauseam:
-        "adnauseam@rednoise.org" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/adnauseam/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        # Sponsor Block:
-        "sponsorBlocker@ajay.app" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
-          installation_mode = "force_installed";
-        };
       };
     };
 
