c36fae1b03ac49aa1aa5714b659e7a18493de535
Light3r26 <soria.jacopo@pm.me>
2025-12-05 00:06:50 +0100

    code fix

--- modules/home-manager/apps/floorp.nix
+++ modules/home-manager/apps/floorp.nix
@@ -56,25 +56,24 @@ in
       # "force_installed" and "normal_installed".
       extensions = {
         force = true;
-              settings = {
-          "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+        settings = {
           # Nord Theme:
-          "26e789e7-acf2-4346-9381-ad473c245e43" = {
+          "26e789e7-acf2-4346-9381-ad473c245e43".settings = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/nord-theme/latest.xpi";
             installation_mode = "force_installed";
           };
           # Dark Reader:
-          "addon@darkreader.org" = {
+          "addon@darkreader.org".settings = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
             installation_mode = "force_installed";
           };
           # AdNauseam:
-          "adnauseam@rednoise.org" = {
+          "adnauseam@rednoise.org".settings = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/adnauseam/latest.xpi";
             installation_mode = "force_installed";
           };
           # Sponsor Block:
-          "sponsorBlocker@ajay.app" = {
+          "sponsorBlocker@ajay.app".settings = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
             installation_mode = "force_installed";
           };
