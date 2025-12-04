ccd3f4a937119073ae26d850b761f49be2740fc7
Light3r26 <soria.jacopo@pm.me>
2025-12-05 00:00:30 +0100

    added floorp extensions

--- modules/home-manager/apps/floorp.nix
+++ modules/home-manager/apps/floorp.nix
@@ -73,6 +73,38 @@ in
       };
     };
 
+    extensions = {
+      force = true;
+    };
+
+    /* ---- EXTENSIONS ---- */
+    # Check about:support for extension/add-on ID strings.
+    # Valid strings for installation_mode are "allowed", "blocked",
+    # "force_installed" and "normal_installed".
+    ExtensionSettings = {
+      "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+      # Nord Theme:
+      "26e789e7-acf2-4346-9381-ad473c245e43" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/nord-theme/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      # Dark Reader:
+      "addon@darkreader.org" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      # AdNauseam:
+      "adnauseam@rednoise.org" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/adnauseam/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      # Sponsor Block:
+      "sponsorBlocker@ajay.app" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+        installation_mode = "force_installed";
+      };
+    };
+
     /* ---- POLICIES ---- */
     # Check about:policies#documentation for options.
     policies = {
@@ -91,7 +123,7 @@ in
       OverrideFirstRunPage = "";
       OverridePostUpdatePage = "";
       DontCheckDefaultBrowser = true;
-      DisplayBookmarksToolbar = "always"; # alternatives: "never" or "newtab"
+      DisplayBookmarksToolbar = "newtab"; # alternatives: "never" or "always"
       DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
       SearchBar = "unified"; # alternative: "separate"
     };
