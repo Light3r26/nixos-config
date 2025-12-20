6729f27bedaf72e828c3eb822bf5614533bd9199
Light3r26 <soria.jacopo@pm.me>
2025-12-20 23:48:10 +0100

    fix: adding vboxnet interfaces to the unmanaged list to have faster boot

--- hosts/msi-laptop/system-modules/networking.nix
+++ hosts/msi-laptop/system-modules/networking.nix
@@ -14,6 +14,9 @@
       "192.168.1.56" = ["home-server"];
     };
 
+    # Needed to have faster boot with NixOS virtualbox module active
+    networkmanager.unmanaged = [ "interface-name:vboxnet*" ];
+
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/"; 
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
