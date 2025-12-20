809d93e75e85a90cde4278c41972981e2a474480
Light3r26 <soria.jacopo@pm.me>
2025-12-20 23:57:58 +0100

    fix: should reduce boot time

--- hosts/msi-laptop/system-modules/networking.nix
+++ hosts/msi-laptop/system-modules/networking.nix
@@ -14,9 +14,6 @@
       "192.168.1.56" = ["home-server"];
     };
 
-    # Needed to have faster boot with NixOS virtualbox module active
-    networkmanager.unmanaged = [ "interface-name:vboxnet*" ];
-
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/"; 
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
--- modules/nixos/msi-laptop/virtualization.nix
+++ modules/nixos/msi-laptop/virtualization.nix
@@ -7,4 +7,7 @@
   };
 
   users.extraGroups.vboxusers.members = [ "light3r" ];
+
+  # Needed to have faster boot with NixOS virtualbox module active
+  systemd.services.NetworkManager-wait-online.enable = false; #Disables the start job for vboxnet0
 }
