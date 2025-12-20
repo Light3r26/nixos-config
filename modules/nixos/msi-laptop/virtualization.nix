065060f873d1bb8726a547f86de15274ae5d5453
Light3r26 <soria.jacopo@pm.me>
2025-12-21 00:10:50 +0100

    reduced timeout for vnm-wait-online service (targeting vboxnet0) to 1s

--- modules/nixos/msi-laptop/virtualization.nix
+++ modules/nixos/msi-laptop/virtualization.nix
@@ -9,5 +9,5 @@
   users.extraGroups.vboxusers.members = [ "light3r" ];
 
   # Needed to have faster boot with NixOS virtualbox module active
-  systemd.services.NetworkManager-wait-online.enable = false; #Disables the start job for vboxnet0
+  systemd.services.NetworkManager-wait-online.serviceConfig.TimeoutSec = "1s";
 }
