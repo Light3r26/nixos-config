7a4ea5f7a020b93df2ef3791dd99aff147e27a0d
Light3r26 <soria.jacopo@pm.me>
2026-02-10 23:20:45 +0100

    added hyprpanel config

--- modules/home-manager/desktop/hyprpanel.nix
+++ modules/home-manager/desktop/hyprpanel.nix
@@ -2,5 +2,36 @@
   programs.hyprpanel = {
     enable = true;
     systemd.enable = true;
+    settings = {
+      layout = {
+        bar.layouts = {
+          "*" = {
+            left = [ "dashboard" "cpu" "ram" "battery" "media"];
+            middle = [ "workspaces" ];
+            right = [ "wifi" "bluetooth" "volume" "systray" "notifications" ];
+          };
+        };
+      };
+
+      bar.launcher.autoDetectIcon = true;
+
+      menus.clock = {
+        time = {
+          military = true;
+          hideSeconds = true;
+        };
+        weather.unit = "metric";
+      };
+
+      menus.dashboard.directories.enabled = false;
+      menus.dashboard.stats.enable_gpu = true;
+
+      theme.bar.transparent = true;
+
+      theme.font = {
+        name = "CaskaydiaCove NF";
+        size = "16px";
+      };
+    };
   };
 }
