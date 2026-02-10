5736526734e98cded7a576a904803b9bda4d98a1
Light3r26 <soria.jacopo@pm.me>
2026-02-11 00:32:30 +0100

    temporarily disabled hyprpanel home manager because of config conflicts

--- modules/home-manager/desktop/hyprpanel.nix
+++ modules/home-manager/desktop/hyprpanel.nix
@@ -1,37 +1,39 @@
+{ pkgs, ... }:
 {
-  programs.hyprpanel = {
-    enable = true;
-    systemd.enable = true;
-    settings = {
-      layout = {
-        bar.layouts = {
-          "*" = {
-            left = [ "dashboard" "cpu" "ram" "battery" "media"];
-            middle = [ "workspaces" ];
-            right = [ "wifi" "bluetooth" "volume" "systray" "notifications" ];
-          };
-        };
-      };
+  environment.systemPackages = [ pkgs.hyprpanel ];
+ #programs.hyprpanel = {
+ #  enable = true;
+ #  systemd.enable = true;
+ #  settings = {
+ #    layout = {
+ #      bar.layouts = {
+ #        "*" = {
+ #          left = [ "dashboard" "cpu" "cputemp" "ram" "media"];
+ #          middle = [ "workspaces" ];
+ #          right = [ "wifi" "bluetooth" "battery" "volume" "notifications" ];
+ #        };
+ #      };
+ #    };
 
-      bar.launcher.autoDetectIcon = true;
+ #    bar.launcher.autoDetectIcon = true;
 
-      menus.clock = {
-        time = {
-          military = true;
-          hideSeconds = true;
-        };
-        weather.unit = "metric";
-      };
+ #    menus.clock = {
+ #      time = {
+ #        military = true;
+ #        hideSeconds = true;
+ #      };
+ #      weather.unit = "metric";
+ #    };
 
-      menus.dashboard.directories.enabled = false;
-      menus.dashboard.stats.enable_gpu = true;
+ #    menus.dashboard.directories.enabled = false;
+ #    menus.dashboard.stats.enable_gpu = true;
 
-      theme.bar.transparent = true;
+ #    theme.bar.transparent = false;
 
-      theme.font = {
-        name = "CaskaydiaCove NF";
-        size = "16px";
-      };
-    };
-  };
+ #    theme.font = {
+ #      name = "CaskaydiaCove NF";
+ #      size = "16px";
+ #    };
+ #  };
+ #};
 }
