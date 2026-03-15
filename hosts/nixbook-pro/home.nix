65a2b64821b0149e8153e29ec268adf55f3914c3
light3r <light3r@nixbook-pro>
2026-03-15 13:45:56 +0100

    gesture fix

--- hosts/nixbook-pro/home.nix
+++ hosts/nixbook-pro/home.nix
@@ -32,8 +32,7 @@
     enable = true;
     monitors = [ "eDP-1, preferred, auto, 1.25" ];
     gestures = [
-      "3, left, workspace"
-      "3, right, workspace"
+      "3, horizontal, workspace"
     ];
   };
   hyprpanel = {
