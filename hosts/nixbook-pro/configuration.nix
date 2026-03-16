9bf35952771a24abd18e95ff4419c044be12c48a
light3r <light3r@nixbook-pro>
2026-03-16 18:54:50 +0100

    enabled virtualization on nixbook-pro

--- hosts/nixbook-pro/configuration.nix
+++ hosts/nixbook-pro/configuration.nix
@@ -90,7 +90,7 @@
   gaming.enable = true;
   servers.enable = false;
   sddm.enable = true;
-  virtualization.enable = false;
+  virtualization.enable = true;
   bootloader = {
     enable = true;
     multiBootSupport = false;
