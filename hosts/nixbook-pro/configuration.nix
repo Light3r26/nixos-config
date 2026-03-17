331a89e625cbf7e87946c8f6639b0d6f995d6b22
light3r <light3r@nixbook-pro>
2026-03-17 07:50:05 +0100

    removing nvidia environment variables from nixbook-pro config

--- hosts/nixbook-pro/configuration.nix
+++ hosts/nixbook-pro/configuration.nix
@@ -61,9 +61,6 @@
     WLR_NO_HARDWARE_CURSORS = "1";
     # Hint electron apps to use Wayland
     NIXOS_OZONE_WL = "1";
-    LIBVA_DRIVER_NAME = "nvidia";
-    GBM_BACKEND = "nvidia-drm";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
   };
 
    # Allow unfree packages
