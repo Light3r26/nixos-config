8a673106c5598b37d4a62db2f3c452daa7f7d5a1
light3r <light3r@nixbook-pro>
2026-05-16 14:08:12 +0200

    added home-server key to agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,8 @@
 let
   msi-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjkU2SWJD8VdRSdtuSDY61MX673gbLu6rljuzWUhEHC";
   nixbook-pro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHvcTV9j2DcLNQreiBZ7yOLLcbMmBlHfxu+/jdBqI7+t";
-  #remote = "";
-  systems = [ msi-laptop nixbook-pro ];
+  home-server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJFbZsjTW06vV7aqwu+m6y/he1w/yLvF1kGbnHBNrrj/";
+  systems = [ msi-laptop nixbook-pro home-server ];
 
 in
 {
