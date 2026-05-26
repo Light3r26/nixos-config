c9f20a805c6e561f7550ce3169ec46b54f293fd9
Light3r26 <soria.jacopo@pm.me>
2026-05-26 22:03:59 +0200

    using libvirtd for virtualization

--- modules/nixos/msi-laptop/virtualization.nix
+++ modules/nixos/msi-laptop/virtualization.nix
@@ -10,9 +10,22 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    virtualization.libvirtd = {
+      enable = true;
+      qemu.vhostUserPackages = [ pkgs.virtiofsd ];
+    };
+    programs.virt-manager.enable = true;
+
     environment.systemPackages = [
-      pkgs.qemu
+      pkgs.dnsmasq
     ];
+
+    users.users.light3r.extraGroups = [ pkgs.libvirtd ];
+
+    environment.sessionVariables = {
+      GDK_BACKEND = "x11 virt-manager";
+    };
+
     virtualisation.virtualbox = {
       host = {
         enable = true;
