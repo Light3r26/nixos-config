9817111e2c91c52ab5fa88d294de24ba3fcf84dd
Light3r26 <soria.jacopo@pm.me>
2026-05-27 22:57:27 +0200

    modifying zen browser config

--- modules/home-manager/apps/zen-browser.nix
+++ modules/home-manager/apps/zen-browser.nix
@@ -14,48 +14,84 @@ in
   config = lib.mkIf cfg.enable {
     programs.zen-browser = {
       enable = true;
+      setAsDefaultBrowser = true;
       languagePacks = [ "it" "en-GB" ];
 
       /* ---- PROFILES ---- */
-      profiles.Light3r = {
-        name = "Light3r";
-
-        search = {
-          force = true;
-          default = "startpage";
-          engines = {
-            "searx" = {
-              urls = [{
-                template = "https://search.jacoposoria.qzz.io/?q={searchTerms}";
-                params = [
-                  { name = "query"; value = "{searchTerms}"; }
-                ];
-              }];
-              icon = "https://docs.searxng.org/_static/searxng-wordmark.svg";
-              definedAliases = [ "@sx" ];
+      profiles = {
+        default = {
+          containersForce = true;
+          containers = {
+            Personal = {
+              color = "green";
+              icon = "fingerprint";
+              id = 1;
             };
-
-            "startpage" = {
-              urls = [{
-                template = "https://www.startpage.com/sp/search?q={searchTerms}";
-                params = [
-                  { name = "query"; value = "{searchTerms}"; }
-                ];
-              }];
-              icon = "https://www.startpage.com/favicon.ico";
-              definedAliases = [ "@sp" ];
+            Work = {
+              color = "blue";
+              icon = "briefcase";
+              id = 2;
             };
+            Shopping = {
+              color = "yellow";
+              icon = "dollar";
+              id = 3;
+            };
+          };
 
-            "Nix Packages" = {
-              urls = [{
-                template = "https://search.nixos.org/packages";
-                params = [
-                  { name = "type"; value = "packages"; }
-                  { name = "query"; value = "{searchTerms}"; }
-                ];
-              }];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@np" ];
+        spacesForce = true;
+        spaces = {
+          "Personale" = {
+            id = "l4b62ty7-386n-4015-hk9v-s2w0a257c93p";
+            position = 1000;
+            icon = "🏠";
+          };
+          "Scuola" = {
+            id = "tln97shz-2tg0-163l-84v5-a10m330y817c";
+            position = 2000;
+            icon = "📚";
+          };
+        };
+        Light3r = {
+          name = "Light3r";
+
+          search = {
+            force = true;
+            default = "startpage";
+            engines = {
+              "searx" = {
+                urls = [{
+                  template = "https://search.jacoposoria.qzz.io/?q={searchTerms}";
+                  params = [
+                    { name = "query"; value = "{searchTerms}"; }
+                  ];
+                }];
+                icon = "https://docs.searxng.org/_static/searxng-wordmark.svg";
+                definedAliases = [ "@sx" ];
+              };
+
+              "startpage" = {
+                urls = [{
+                  template = "https://www.startpage.com/sp/search?q={searchTerms}";
+                  params = [
+                    { name = "query"; value = "{searchTerms}"; }
+                  ];
+                }];
+                icon = "https://www.startpage.com/favicon.ico";
+                definedAliases = [ "@sp" ];
+              };
+
+              "Nix Packages" = {
+                urls = [{
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    { name = "type"; value = "packages"; }
+                    { name = "query"; value = "{searchTerms}"; }
+                  ];
+                }];
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@np" ];
+              };
             };
           };
         };
@@ -104,8 +140,30 @@ in
             sponsorblock
             youtube-shorts-block
             bitwarden
+            vimium
           ];
         };
+
+        policies = {
+          AutofillAddressEnabled = true;
+          AutofillCreditCardEnabled = false;
+          DisableAppUpdate = true;
+          DisableFeedbackCommands = true;
+          DisableFirefoxStudies = true;
+          DisablePocket = true;
+          DisableTelemetry = true;
+          DontCheckDefaultBrowser = true;
+          NoDefaultBookmarks = true;
+          OfferToSaveLogins = false;
+          EnableTrackingProtection = {
+            Value = true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+          };
+        };
+
+        # Zen-specific settings
       };
     };
   };
