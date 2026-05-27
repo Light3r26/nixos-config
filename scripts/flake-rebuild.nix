0e82ec3517d424129c345743663ccb3de29fe89b
Light3r26 <soria.jacopo@pm.me>
2026-05-27 23:29:03 +0200

    trying it another time

--- scripts/flake-rebuild.nix
+++ scripts/flake-rebuild.nix
@@ -9,14 +9,10 @@ pkgs.writeShellScriptBin "flake-rebuild" ''
   git diff
 
   # Git commit
-  if [ "$@" =~ "--no-commit"  ]; then
-    echo -ne "\033[4;32mAssicurati di eseguire il commit alla fine del rebuild:\033[0m "
-  else
-    git add .
-    echo -ne "\033[4;32mInserisci messaggio del commit:\033[0m " # ANSI Text
-    read -r commit_message
-    git commit -m "$commit_message"
-  fi
+  git add .
+  echo -ne "\033[4;32mInserisci messaggio del commit:\033[0m " # ANSI Text
+  read -r commit_message
+  git commit -m "$commit_message"
 
   # System rebuild
   sudo nixos-rebuild "$1" --flake .#"$2"
