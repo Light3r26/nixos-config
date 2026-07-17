b1347b23b44ba821c85ef8f85e73251c582041ca
Light3r26 <soria.jacopo@pm.me>
2026-07-17 18:58:52 +0200

    feat: toggleable freshrss module

--- hosts/home-server/services/freshrss.nix
+++ hosts/home-server/services/freshrss.nix
@@ -1,28 +1,38 @@
-{config, ...}:
+{ lib, config, ...}:
 
+let
+  cfg = config.freshrss;
+
+in
 {
-  services.freshrss = {
-    enable = true;
-    baseUrl = "/";
-    virtualHost = "rss.jacoposoria.qzz.io";
-    language = "it";
-    #defaultUser = "light3r";
-    passwordFile = config.age.secrets."freshrss-key.age".path;
+  options.freshrss = {
+    enable = lib.mkEnableOption "Enable FreshRSS";
   };
 
-  age.secrets."freshrss-key.age" = {
-    file = "/Nixos/secrets/freshrss-key.age";
-    owner = "freshrss";
-    group = "freshrss";
-    mode = "0640";
-  };
+  config = lib.mkIf cfg.enable {
+    services.freshrss = {
+      enable = true;
+      baseUrl = "/";
+      virtualHost = "rss.jacoposoria.qzz.io";
+      language = "it";
+      #defaultUser = "light3r";
+      passwordFile = config.age.secrets."freshrss-key.age".path;
+    };
 
-  services.nginx.virtualHosts."rss.jacoposoria.qzz.io" = {
-    serverName = "rss.jacoposoria.qzz.io";
-    listen = [ { addr = "127.0.0.1"; port = 80; } ];
-    locations."/" = {
-      #proxyPass = "http://127.0.0.1:port?";
-      proxyWebsockets = true;
+    age.secrets."freshrss-key.age" = {
+      file = "/Nixos/secrets/freshrss-key.age";
+      owner = "freshrss";
+      group = "freshrss";
+      mode = "0640";
+    };
+
+    services.nginx.virtualHosts."rss.jacoposoria.qzz.io" = {
+      serverName = "rss.jacoposoria.qzz.io";
+      listen = [ { addr = "127.0.0.1"; port = 80; } ];
+      locations."/" = {
+        #proxyPass = "http://127.0.0.1:port?";
+        proxyWebsockets = true;
+      };
     };
   };
 }
