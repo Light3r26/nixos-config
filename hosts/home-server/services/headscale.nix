3045e58901f2378bd44a48112056ee55b8775837
nixos@home-server <soria.jacopo@pm.me>
2026-08-09 17:23:50 +0200

    feat: added traefik configuration to headscale module

--- hosts/home-server/services/headscale.nix
+++ hosts/home-server/services/headscale.nix
@@ -1,26 +1,30 @@
-let
-  domain = "jacoposoria.it";
-
-in
 {
   services.headscale = {
     enable = true;
     port = 3492;
     settings = {
-      server_url = "https://headscale.${domain}";
-      logtail.enabled = true;
-
+      server_url = "https://headscale.jacoposoria.it";
       dns = {
-        base_domain = domain;
-        nameservers = {
-	  global = [
-            "1.1.1.1"
-	    "1.0.0.1"
-	    "2606:4700:4700::1111"
-	    "2606:4700:4700::1001"
-	  ];
-	};
+        base_domain = "local";
       };
+      logtail.enabled = true;
+    };
+  };
+
+  services.traefik.dynamicConfigOptions.http = {
+    services.headscale.loadBalancer.servers = [
+      {
+        url = "http://localhost:3492";
+      }
+    ];
+    
+    routers.headscale = {
+      rule = "Host(`headscale.jacoposoria.it`)";
+      tls = {
+	certResolver = "ionos";
+      };
+      service = "headscale";
+      entrypoints = [ "websecure" ];
     };
   };
 }
