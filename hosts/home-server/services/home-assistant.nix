7b76b242d12f58bee261da910d083bc2819a5161
nixos@home-server <soria.jacopo@pm.me>
2026-08-14 15:19:05 +0200

    change: switching home assistant to container mode as core installation (used in nixos module) isn't supported anymore

--- hosts/home-server/services/home-assistant.nix
+++ hosts/home-server/services/home-assistant.nix
@@ -1,27 +1,19 @@
 { config, ... }:
 
 {
-  services.home-assistant = {
-    enable = true;
-    # Default port is 8123
-    extraComponents = [
-      # Components required to complete the onboarding
-      "analytics"
-      "google_translate"
-      "met"
-      "radio_browser"
-      "shopping_list"
-      # Used for fast zlib compression
-      "isal"     
-    ];
-    config = {
-      # Includes dependencies for a basic setup
-      # https://www.home-assistant.io/integrations/default_config/
-      default_config = {};
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.homeassistant = {
+      volumes = [ "home-assistant:/config" ];
+      environment.TZ = "Europe/Rome";
+      # Note: The image will not be updated on rebuilds, unless the version label changes
+      image = "ghcr.io/home-assistant/home-assistant:stable";
+      extraOptions = [ 
+        # Use the host network namespace for all sockets
+        "--network=host"
+        # Pass devices into the container, so Home Assistant can discover and make use of them
+        "--device=/dev/ttyACM0:/dev/ttyACM0"
+      ];
     };
   };
-
-  networking.firewall.allowedTCPPorts = [
-    config.services.home-assistant.config.http.server_port
-  ];
 }
