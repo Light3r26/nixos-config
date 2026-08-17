5f2b4a97e7dc8839234019511a71d4b5259b8fc9
light3r <light3r@nixbook-pro>
2026-08-17 23:33:06 +0200

    feat: transforming mountain server into a tailscale subnet router

--- hosts/mountain-server/system-modules/tailscale.nix
+++ hosts/mountain-server/system-modules/tailscale.nix
@@ -4,9 +4,11 @@
   config.services.tailscale = {
     enable = true;
     authKeyFile = config.age.secrets."mountain-server-tailscale-key.age".path;
+    useRoutingFeatures = "server";
     extraUpFlags = [
       "--login-server=https://headscale.jacoposoria.it"
       "--accept-routes"
+      "--advertise-routes=192.168.1.0/24"
     ];
   };
   config.age.secrets."mountain-server-tailscale-key.age".file = "/Nixos/secrets/mountain-server-tailscale-key.age";
