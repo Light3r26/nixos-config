bf0693777125fa44e9c81d33cfe6918cef91ccc2
light3r <light3r@nixbook-pro>
2026-08-18 13:02:12 +0200

    fix: allowing hosts for tandoor

--- hosts/home-server/services/tandoor.nix
+++ hosts/home-server/services/tandoor.nix
@@ -5,6 +5,7 @@
     port = 3301;
     extraConfig = {
       MEDIA_ROOT = "/var/lib/tandoor-recipes/media";
+      ALLOWED_HOSTS = "localhost,home-server.local";
       ENABLE_SIGNUP = "1";
     };
   };
