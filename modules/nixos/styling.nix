b38cdaab69180b57dc5697f0aabdef30f2fccb39
Light3r26 <soria.jacopo@pm.me>
2026-08-21 10:13:14 +0200

    feat: keeping one single module with both home manager and nixos options for styling

--- modules/nixos/styling.nix
+++ modules/nixos/styling.nix
@@ -1,37 +1,46 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-  stylix = {
-    enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
-    image = ../../.assets/wallpapers/dark-bright-mountains.jpg;
+  config = {
+    stylix = {
+      enable = true;
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+      image = ../../.assets/wallpapers/dark-bright-mountains.jpg;
 
-    cursor = {
-      package = pkgs.whitesur-cursors;
-      name = "WhiteSur-cursors";
-      size = 24;
+      cursor = {
+        package = pkgs.whitesur-cursors;
+        name = "WhiteSur-cursors";
+        size = 24;
+      };
+
+      fonts = {
+        monospace = {
+          package = pkgs.nerd-fonts.caskaydia-cove;
+          name = "CaskaydiaCove Nerd Font Mono";
+        };
+
+        serif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Serif";
+        };
+
+        sansSerif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Sans";
+        };
+      };
+
+      opacity.terminal = 0.7;
+      targets = {
+        grub.enable = false;
+      };
     };
 
-    fonts = {
-      monospace = {
-        package = pkgs.nerd-fonts.caskaydia-cove;
-        name = "CaskaydiaCove Nerd Font Mono";
+    home-manager.users.light3r = {
+      stylix.targets = {
+        starship.enable = false;
+        zen-browser.enable = false;
       };
-
-      serif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Serif";
-      };
-
-      sansSerif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans";
-      };
-    };
-
-    opacity.terminal = 0.7;
-    targets = {
-      grub.enable = false;
     };
   };
 }
