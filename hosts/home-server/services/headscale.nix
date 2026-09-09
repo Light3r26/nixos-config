412f15ee99f5238ff2393a8f6071d025541b16ac
nixos@home-server <soria.jacopo@pm.me>
2026-09-10 01:21:09 +0200

    fix: added missin ;

--- hosts/home-server/services/headscale.nix
+++ hosts/home-server/services/headscale.nix
@@ -34,7 +34,7 @@ in
       routers.headscale = {
         rule = "host(`headscale.jacoposoria.it`)";
         tls.certresolver = "ionos";
-	service = "headscale"
+	service = "headscale";
         entrypoints = [ "websecure" ];
       };
     };
